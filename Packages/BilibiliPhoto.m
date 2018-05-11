fd89b772d1b37d82e7479c7a7e49e7291a356fd5
GalAster <galaster@foxmail.com>
2018-05-11 17:16:59 +0800

    函数::PhotosLeaderboard, 获取当前的图像作品排行榜

--- Packages/BilibiliPhoto.m
+++ Packages/BilibiliPhoto.m
@@ -17,31 +17,19 @@
 (*4.如果修改了源代码，包含一份代码修改说明。*)
 (* ::Section:: *)
 (*函数说明*)
-BilibiliArticle::usage = "将HTML转化为Markdown格式.";
+PicturesPack::ussage="内部函数, 图片地址对象.";
+PhotosLeaderboard::ussage="h.bilibili.com 图片作品排行榜.";
 (* ::Section:: *)
 (*程序包正体*)
 Begin["`Photos`"];
 (* ::Subsection::Closed:: *)
-(*主体代码*)
-PictureDataRebuild::ussage="内部函数, 用于数据清洗";
-PictureDataRebuild[doc_Association]:=<|
-	"uid"->doc["user","uid"],
-	"author"->doc["user","name"],
-	"did"->doc["item","doc_id"],
-	"title"->doc["item","title"],
-	"time"->FromUnixTime[doc["item","upload_time"]],
-	"imgs"->("img_src"/.doc["item","pictures"]),
-	"size"->If[KeyExistsQ[First@doc["item","pictures"],"img_size"],
-		N@Quantity[Total["img_size"/.doc["item","pictures"]],"Megabytes"],
-		Missing
-	]
-|>;
-
-
-(* ::Subsection::Closed:: *)
 (*PicturesPackPbject*)
+PicturesPackQ::ussage="PicturesPack 合法性检测";
+
 PicturesPackQ[asc_?AssociationQ] := AllTrue[{"Date","Count"}, KeyExistsQ[asc, #]&]
 PicturesPackQ[_] = False;
+Format[PicturesPack[___],OutputForm]:="PicturesPack[<>]";
+Format[PicturesPack[___],InputForm]:="PicturesPack[<>]";
 PicturesPack/:MakeBoxes[obj:PicturesPack[asc_?PicturesPackQ],form:(StandardForm|TraditionalForm)]:=Module[
 	{above,below},
 	above={
@@ -63,6 +51,41 @@ PicturesPack/:MakeBoxes[obj:PicturesPack[asc_?PicturesPackQ],form:(StandardForm|
 		form,
 		"Interpretable" -> Automatic
 	]
+];
+PicturesPack[ass_]["Data"]:=Lookup[ass,"Data"];
+
+
+
+
+(* ::Subsection::Closed:: *)
+(*PhotosLeaderboard*)
+PictureDataRebuild::ussage="内部函数, 用于数据清洗";
+PictureDataRebuild[doc_Association]:=<|
+	"uid"->doc["user","uid"],
+	"author"->doc["user","name"],
+	"did"->doc["item","doc_id"],
+	"title"->doc["item","title"],
+	"time"->FromUnixTime[doc["item","upload_time"]],
+	"imgs"->("img_src"/.doc["item","pictures"]),
+	"size"->If[KeyExistsQ[First@doc["item","pictures"],"img_size"],
+		N@Quantity[Total["img_size"/.doc["item","pictures"]],"Megabytes"],
+		Missing
+	]
+|>;
+$PhotoMap=<|"Cosplay"->"cos","其他服饰"->"sifu","插画"->"illustration","漫画"->"comic","其他画作"->"draw","全部画作"->"all"|>;
+PhotosLeaderboard[cat_]:=Module[
+	{$now=Now,raw,data},
+	raw=URLExecute[HTTPRequest[#,TimeConstraint->10],"RawJSON"]["data","items"]&/@$APIs["hot"][$PhotoMap[cat]];
+	data=PictureDataRebuild/@Flatten[raw];
+	PicturesPack[<|
+		"data"->data,
+		"Category"->Text@cat,
+		"Repo"->Length@data,
+		"Count"->Length@Flatten["imgs"/.data],
+		"Size"->Total@DeleteCases["size"/.data,Missing],
+		"Time"->Now-$now,
+		"Date"->$now
+	|>]
 ];
 
 
