466fe156c6a420e8afb46a360fcd5a6e3d777ae2
GalAster <galaster@foxmail.com>
2018-07-27 12:37:41 +0800

    模块::测试POST请求

--- Packages/__Dev/Proxy.m
+++ Packages/__Dev/Proxy.m
@@ -37,6 +37,26 @@ TagDown[i_, try_] := Block[
 	finish++;
 	TagFormat[get["data"]]
 ];
+
+
+GetMemberRequest[mid_] := HTTPRequest[
+	"https://space.bilibili.com/ajax/member/GetInfo",
+	<|
+		"Method" -> "POST",
+		"Headers" -> {
+			"content-type" -> "application/x-www-form-urlencoded;charset=UTF-8",
+			"user-agent" -> "Mozilla/5.0 (X11;Linux i686) AppleWebKit/534.24 (KHTML,like Gecko) Chrome/11.0.696.68 Safari/534.24",
+			"Referer" -> "https://space.bilibili.com/" <> ToString[mid]
+		},
+		"Body" -> "mid=" <> ToString[mid] <> "&csrf="
+	|>,
+	TimeConstraint -> 10
+];
+GetReg[id_] := Quiet@Block[
+	{get = URLExecute[GetMemberRequest[id], "RawJSON"]},
+	DateString[FromUnixTime[get["data", "regtime"]], {"Year", "Month", "Day"}] -> id
+]
+
 End[] (* `Private` *)
 
 EndPackage[]
