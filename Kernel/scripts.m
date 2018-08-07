d5ad6bf72d56d112fd71c7b5a752fb01bb3c0b8d
GalAster <galaster@foxmail.com>
2018-08-08 07:23:00 +0800

    函数::ArticleCrawlers 移植

--- Kernel/scripts.m
+++ Kernel/scripts.m
@@ -1,21 +1,21 @@
 (* ::Package:: *)
 
-BScript`Pack[]:=Module[
-	{add,info,paclet},
-	add[v_]:=Block[
-		{p,sp=StringSplit[v,"."]},
-		p=ToString[ToExpression[Last@sp]+1];
-		StringRiffle[Append[Most@sp,p],"."]
+BScript`Pack[] := Module[
+	{add, info, paclet},
+	add[v_] := Block[
+		{p, sp = StringSplit[v, "."]},
+		p = ToString[ToExpression[Last@sp] + 1];
+		StringRiffle[Append[Most@sp, p], "."]
 	];
-	info=FindFile["BilibiliLink/PacletInfo.m"];
-	paclet=Import[info]/.{("Version"->v__)->("Version"->Inactive[add][v])}//Activate;
-	Export[info,StringReplace[ToString[paclet,InputForm],
+	info = FindFile["BilibiliLink/PacletInfo.m"];
+	paclet = Import[info] /. {("Version" -> v__) -> ("Version" -> Inactive[add][v])} // Activate;
+	Export[info, StringReplace[ToString[paclet, InputForm],
 		{
-			"["->"[\n","]"->"\n]",
-			"{"->"{\n","}"->"\n}",
-			","->",\n"
+			"[" -> "[\n", "]" -> "\n]",
+			"{" -> "{\n", "}" -> "\n}",
+			"," -> ",\n"
 		}
-	],"Text"];
+	], "Text"];
 	PacletInformation[PackPaclet["BilibiliLink"]]
 ];
 
